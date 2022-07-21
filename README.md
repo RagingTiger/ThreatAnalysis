ffcd8ce5682b65b54f4acd31a6b4a2e706c73f61
RagingTiger <yourmom@example.com>
2022-07-21 11:16:56 -0400

    MVP

---
+++ README.md
@@ -0,0 +1,4 @@
+![pytest](https://github.com/RagingTiger/ThreatAnalysis/actions/workflows/pytest.yml/badge.svg)
+# TL;DR
+Rough attempt at developing *threat analysis algorithm* for determining
+relevance of various *threats*.
---
+++ threat_analysis.py
@@ -0,0 +1,28 @@
+# classes
+class Threat():
+    """Object to encapsulate threat information"""
+    def __init__(self,
+                 name: str,
+                 probability: float,
+                 impact: float,
+                 mvp: str) -> None:
+        self.name = name
+        self.probability = probability
+        self.impact = impact
+        self.mvp = mvp
+
+    def __repr__(self) -> str:
+        """Printable string representation of Threat() object"""
+        return '<{0}.{1} object at {2}>'.format(
+            type(self).__module__, type(self).__qualname__, hex(id(self))
+        )
+
+    def __str__(self) -> str:
+        """Readable string representation of Threat() object"""
+        return f'Threat({self.name}): p={self.probability}/i={self.impact})'
+
+
+# functions
+def rank(threats: list[Threat]) -> list[Threat]:
+    """Return a sorted listed based on threat probability"""
+    return sorted(threats, key=lambda t: t.probability * t.impact, reverse=True)
